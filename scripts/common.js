07d7038f7f8ea1c7229540f2b36bf16633e6ba13
Pratap <pratap@techcetra.com>
2017-07-07 15:50:26 +0530

    changes

--- scripts/common.js
+++ scripts/common.js
@@ -9,3 +9,35 @@ function showPageLoader(){
 function hidePageLoader(){
   $('.loader').hide();
 }
+
+var trigger = '.form-layout .form-field input,.form-layout .form-field select,.form-layout .form-field textarea,.form-horizontal .form-group  input,.form-horizontal .form-group  select,.form-horizontal .form-group textarea';
+$(window).load(function () {
+  $(document).on('focus','.form-layout .form-field input,.form-layout .form-field select,.form-layout .form-field textarea,.form-horizontal .form-group  input,.form-horizontal .form-group  select,.form-horizontal .form-group textarea',function () {
+    var target = $(this).parents('.form-field,.form-group');
+    target.addClass('focused');
+  });
+  $('.form-layout .form-field,.form-horizontal .form-group').each(function () {
+    var target  = $(this);
+    var input = $(this).find('input,select,textarea');
+    if(input.val() == '' || input.val() == 'null' || input.val() == 'undefined'){
+      target.removeClass('focused')
+    }
+    else{
+      if(input.attr('type') == 'file'){
+        upadteFileName($(input))
+      }
+      target.addClass('focused')
+    }
+  })
+  $(document).on('blur',trigger,function () {
+    var target = $(this).parents('.form-field,.form-group');
+    var input = $(this);
+    if(input.val() == '' || input.val() == 'null' || input.val() == 'undefined' || input.val() == '(an empty string)'){
+      target.removeClass('focused')
+    }
+    else{
+      target.addClass('focused')
+    }
+  })
+
+});
